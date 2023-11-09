634e2cbff50e7ed2b0096c1549fc957dd2dd4ac0
messaoudimaher <messaoudimaher1919@gmail.com>
2023-11-08 22:18:36 -0500

    Facial landmarks

--- utils/functions.py
+++ utils/functions.py
@@ -161,11 +161,11 @@ def cv_draw_landmark(img_ori, pts, box=None, color=GREEN, size=1):
     n = pts.shape[1]
     if n <= 106:
         for i in range(n):
-            cv2.circle(img, (int(round(pts[0, i])), int(round(pts[1, i]))), size, color, -1)
+            cv2.line(img, (int(round(pts[0, i])), int(round(pts[1, i]))), size, color, -1)
     else:
         sep = 1
         for i in range(0, n, sep):
-            cv2.circle(img, (int(round(pts[0, i])), int(round(pts[1, i]))), size, color, 1)
+            cv2.line(img, (int(round(pts[0, i])), int(round(pts[1, i]))), size, color, 1)
 
     if box is not None:
         left, top, right, bottom = np.round(box).astype(np.int32)
